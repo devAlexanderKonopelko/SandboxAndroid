b02a3567e4526a2fac01a9d7d694894bd92362ea
Alexander Konopelko <konopelko99@mail.ru>
2021-07-30 13:45:13 +0300

    feat: added gradle dependencies and template classes

---
+++ app/src/main/java/com/konopelko/sandboxandroid/presentation/view/home/HomeActivity.kt
@@ -0,0 +1,6 @@
+package com.konopelko.sandboxandroid.presentation.view.home
+
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity: AppCompatActivity() {
+}
---
+++ app/src/main/java/com/konopelko/sandboxandroid/presentation/viewmodel/HomeViewModel.kt
@@ -0,0 +1,8 @@
+package com.konopelko.sandboxandroid.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel: ViewModel() {
+
+
+}
