7a2c8c7d50c66371f5b21c1307b232a7f895a49e
Alexander Konopelko <konopelko99@mail.ru>
2021-08-02 16:02:43 +0300

    fix: updated home view model live data

--- app/src/main/java/com/konopelko/sandboxandroid/presentation/viewmodel/home/HomeViewModel.kt
+++ app/src/main/java/com/konopelko/sandboxandroid/presentation/viewmodel/home/HomeViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.*
 import com.konopelko.sandboxandroid.data.api.entity.response.NewsResponse
 import com.konopelko.sandboxandroid.domain.usecase.getnews.GetAndroidNewsUseCase
 import com.konopelko.sandboxandroid.utils.disposable.addToSubscriptions
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.Disposable
 import javax.inject.Inject
 
@@ -13,20 +12,17 @@ class HomeViewModel @Inject constructor(
     getAndroidNews: GetAndroidNewsUseCase
 ) : ViewModel() {
 
-    private val _articles = MutableLiveData<List<NewsResponse.Article>>()
-    val articles: LiveData<List<NewsResponse.Article>> = _articles
-
-    private val _dataIsLoading = MutableLiveData<Boolean>()
-    val dataIsLoading: LiveData<Boolean> = _dataIsLoading
+    private val articles = MutableLiveData<List<NewsResponse.Article>>()
+    private val dataIsLoading = MutableLiveData<Boolean>()
 
     private val subscriptions = mutableListOf<Disposable>()
 
     init {
-        _dataIsLoading.value = true
+        dataIsLoading.value = true
         getAndroidNews()
             .subscribe({
-                _dataIsLoading.postValue(false)
-                _articles.postValue(it.articles)
+                dataIsLoading.postValue(false)
+                articles.postValue(it.articles)
             }, {
                 Log.e("News ", "error")
                 it.printStackTrace()
@@ -47,4 +43,8 @@ class HomeViewModel @Inject constructor(
     fun setupLifecycleOwner(viewLifecycleOwner: LifecycleOwner) {
         viewLifecycleOwner.lifecycle.addObserver(lifecycleObserver)
     }
+
+    fun getArticles(): LiveData<List<NewsResponse.Article>> = articles
+
+    fun getDataIsLoading(): LiveData<Boolean> = dataIsLoading
 }
