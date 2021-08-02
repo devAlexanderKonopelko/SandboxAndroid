418385ad73b4752187d0fd6de7a79cefdd0deff7
Alexander Konopelko <konopelko99@mail.ru>
2021-08-02 11:08:20 +0300

    feat: added article item view

--- app/src/main/java/com/konopelko/sandboxandroid/presentation/adapter/news/NewsViewHolder.kt
+++ app/src/main/java/com/konopelko/sandboxandroid/presentation/adapter/news/NewsViewHolder.kt
@@ -3,10 +3,14 @@ package com.konopelko.sandboxandroid.presentation.adapter.news
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.konopelko.sandboxandroid.data.api.entity.response.NewsResponse
+import kotlinx.android.synthetic.main.item_recycler_article.view.*
 
-class NewsViewHolder(view: View): RecyclerView.ViewHolder(view) {
+class NewsViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
 
     fun bind(article: NewsResponse.Article) {
-        //todo: bind article
+        view.articleItemHeadline.text = article.title
+        view.articleItemAuthor.text = article.author
+        view.articleItemDate.text = article.publishedAt
+        view.articleItemText.text = article.description
     }
 }
